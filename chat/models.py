4f99bc692e45cf1c5e76983e6172ff40d10521d4
Vadhel63 <vadhelmilan45@gmail.com>
2024-02-24 16:53:41 +0530

    setted timeone for messaginf according india

--- chat/models.py
+++ chat/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=1000)
 class Message(models.Model):
     value = models.CharField(max_length=1000000)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    date = models.DateTimeField(default=timezone.now, blank=True)
     user = models.CharField(max_length=1000000)
     room = models.CharField(max_length=1000000)
--- djangochat/settings.py
+++ djangochat/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'm%c30v(4=6q!_ouzf5xx9l7n89pl0#l16lo6+z*g4!49-63@t#'
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1","192.168.1.2","localhost"]
+TIME_ZONE = 'Asia/Kolkata'
 
 
 # Application definition
