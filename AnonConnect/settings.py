d73f7d58c7ab8b76e15465b954a64ad91fac822e
Sumit J. Gohil <gohilsumit15@gmail.com>
2024-02-25 11:40:49 +0530

    Added some CSS and Some link for about

--- AnonConnect/settings.py
+++ AnonConnect/settings.py
@@ -60,7 +60,10 @@ ROOT_URLCONF = 'AnonConnect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "about/templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
